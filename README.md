4c0a41d314e77b9d80d803025fb04fbe4881bd3a
muchamadsyaifudin <45529748+muchamadsyaifudin@users.noreply.github.com>
2018-12-09 22:06:52 +0700

    Update and rename README.md to main.cpp

--- README.md
+++
@@ -1,2 +0,0 @@
-# 3-program-do-while-c-
-program c++
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int pilihan = 1;
+	do{
+		cout<<"masukan nomer :";cin>>pilihan;
+	}
+	while(pilihan<=10);
+	return 0;
+}
